d23bbca920b6e131d4e3491ba942274d67d2bee7
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-10-11 14:48:12 -0500

    Se cambió main para agregar un drawer

--- android/app/src/main/java/com/shell/android/registropraxis/ui/main/ui/MainActivity.kt
+++ android/app/src/main/java/com/shell/android/registropraxis/ui/main/ui/MainActivity.kt
@@ -2,21 +2,44 @@ package com.shell.android.registropraxis.ui.main.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.design.widget.NavigationView
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import android.view.View
 import com.shell.android.registropraxis.R
 import com.shell.android.registropraxis.ui.userdata.ui.UserDataActivity
+import com.shell.android.shellcorebaselibrary.CustomToolbarActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+class MainActivity : CustomToolbarActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnUserData.setOnClickListener(this)
+        loadToolbar(toolbarMain as Toolbar)
+        setNavDrawer()
     }
 
     override fun onClick(p0: View?) {
+        startActivity(Intent(this, UserDataActivity::class.java))
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.navUserData -> navigateToUserData()
+        }
+        return true
+    }
+
+    private fun setNavDrawer() {
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, _toolbar, R.string.open_drawer, R.string.close_drawer)
+        toggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    private fun navigateToUserData() {
         startActivity(Intent(this, UserDataActivity::class.java))
     }
 }
